3ad73014d3cd11d46403f60b3792b7d2b4343787
jayk73 <100964538+jayk73@users.noreply.github.com>
2023-06-06 12:48:11 -0500

    Update snowflake.py

--- snowflake.py
+++ snowflake.py
@@ -2,7 +2,6 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
 
 streamlit.title('test title')
 
@@ -37,3 +36,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
